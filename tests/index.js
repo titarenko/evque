2f91bf2fcaf2696e6aabd23389fc6f92a0c626e5
Constantin Titarenko <constantin.titarenko@gmail.com>
2016-02-24 13:18:50 +0200

    fix prefetch test

--- tests/index.js
+++ tests/index.js
@@ -16,37 +16,30 @@ describe('evque', function () {
 	it('should work with specified prefetch', function (done) {
 		var bus = evque('amqp://localhost');
 		bus.on('error', done);
-
-		var counter = 0;
+		
+		var callsCounter = 0;
+		var activeHandlersCounter = 0;
 
 		bus.subscribe('prefetch-event', 'subscriber', function (ev) {
 			return new Promise(function (resolve, reject) {
-				counter++;
+				if (++activeHandlersCounter > 5) {
+					done(new Error('called 6th time before any of previous 5 calls has finished'));
+				}
 				setTimeout(function () {
+					activeHandlersCounter--;
 					resolve();
+					if (++callsCounter == 6) {
+						done();
+					}
 				}, 300);
 			});
-		}, 5).catch(done);
-
-		// At this moment there should be only 5 messages in progress
-		setTimeout(function () {
-			counter.should.be.equal(5);
-		}, 200);
-
-		setTimeout(function () {
-			counter.should.be.equal(6);
-		}, 350);
-
-		// Give last ack some time to be delivered
-		setTimeout(function () {
-			done();
-		}, 650);
-
-		bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
-		bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
-		bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
-		bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
-		bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
-		bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
-	})
+		}, 5).catch(done).then(function () {
+			bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
+			bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
+			bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
+			bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
+			bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
+			bus.publish('prefetch-event', { abba: 'boney m' }).catch(done);
+		});
+	});
 });
